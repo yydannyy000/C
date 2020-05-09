7858f061c20488af6e10a3ad2b7126168dc0f7a2
yydannyy000 <51777101+yydannyy000@users.noreply.github.com>
2020-05-09 20:17:42 +0800

    Add files via upload

---
+++ do while運用/消費金額計算.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int total = 0;
+    int id;
+    do
+    {
+        scanf("%d", &id);
+        switch (id)
+        {
+        case 1:
+            total += 90;
+            break;
+        case 2:
+            total += 75;
+            break;
+        case 3:
+            total += 83;
+            break;
+        case 4:
+            total += 89;
+            break;
+        case 5:
+            total += 71;
+            break;
+        }
+    } while (id != 0);
+    printf("Total: %d\n", total);
+    return 0;
+}
